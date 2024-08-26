a0f2e4380c5b45de2c3a4578225075600791e8b9
Jonathan Juining <73358104+FriesKnight@users.noreply.github.com>
2024-08-27 00:39:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,72 @@
+let currentCardIndex = 0;
+const cards = document.querySelectorAll("#surprise-letter .card");
+
 function handleButtonClick(isYes) {
   const questionDiv = document.getElementById("question");
   const buttonsDiv = document.getElementById("buttons");
   const messageDiv = document.getElementById("message");
   const sendLoveDiv = document.getElementById("sendlove");
+  const surpriseLetterDiv = document.getElementById("surprise-letter");
 
   if (isYes) {
     questionDiv.style.display = "none";
     buttonsDiv.style.display = "none";
 
-    // Show the text message
     messageDiv.innerText =
-      "We are just kidding! Blessed Birthday Nina! Wishing you good health and success! - Love, Airdroitechies ";
+      "We are just kidding! Blessed Birthday Nina! This is just for you! - Love, Airdroitechies ";
+    messageDiv.style.color = "white";
 
-    // Optionally, you can replace the existing gifs with 'dance2.gif'
     sendLoveDiv.innerHTML = '<img src="Images/dance2.gif" alt="Dance 2 GIF">';
 
-    // Trigger the confetti animation
-    confetti({
-      particleCount: 150,
-      spread: 70,
-      origin: { y: 0.6 },
-    });
-  } else {
-    // Change the gif to 'angry.gif'
-    sendLoveDiv.innerHTML = '<img src="Images/angry.gif" alt="Angry GIF">';
+    // Trigger confetti three times with a delay
+    let confettiCount = 0;
+    const confettiInterval = setInterval(() => {
+      confetti({
+        particleCount: 350,
+        spread: 190,
+        origin: { y: 0.5 },
+      });
+      confettiCount++;
 
-    // Optionally, you can hide specific elements or perform other actions
+      // After 3 confetti pops, show the surprise letter
+      if (confettiCount === 3) {
+        clearInterval(confettiInterval);
+        setTimeout(() => {
+          surpriseLetterDiv.style.display = "block";
+          updateCardVisibility();
+        }, 1000); // Delay to allow the last confetti pop to finish
+      }
+    }, 1000); // Adjust the time between each confetti pop (in milliseconds)
+  } else {
+    sendLoveDiv.innerHTML = '<img src="Images/angry.gif" alt="Angry GIF">';
     moveButtonAway();
+  }
+}
+
+function updateCardVisibility() {
+  cards.forEach((card, index) => {
+    card.style.display = "none";
+  });
+
+  cards[currentCardIndex].style.display = "block";
+
+  document.getElementById("backButton").style.display =
+    currentCardIndex > 0 ? "inline-block" : "none";
+  document.getElementById("nextButton").style.display =
+    currentCardIndex < cards.length - 1 ? "inline-block" : "none";
+}
+
+function viewNextCard() {
+  if (currentCardIndex < cards.length - 1) {
+    currentCardIndex++;
+    updateCardVisibility();
+  }
+}
+
+function viewPreviousCard() {
+  if (currentCardIndex > 0) {
+    currentCardIndex--;
+    updateCardVisibility();
   }
 }
 
